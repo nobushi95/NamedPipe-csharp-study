832cedbdefd3ce92eade74c8c782587d9869c278
nobushi95 <nobushi1121@gmail.com>
2023-03-19 17:14:05 +0900

    Pipeクラス実装

--- OneWayConsole/Shared/Pipe.cs
+++ OneWayConsole/Shared/Pipe.cs
@@ -1,10 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.IO.Pipes;
+using System.Reflection;
 
 namespace Shared
 {
     internal class Pipe
     {
+        private static readonly string PipeName = "NamedPipe-csharp-study_OneWayConsole_Pipe";
+
+        public static async Task SendAsync(string msg)
+        {
+            using var pipe = new NamedPipeClientStream(PipeName);
+            try
+            {
+                await pipe.ConnectAsync();
+                using var sw = new StreamWriter(pipe);
+                await sw.WriteLineAsync(msg);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Catch Exception at {MethodBase.GetCurrentMethod()?.Name}(): {ex.Message}");
+            }
+        }
+
+        public static async Task Server(Action<string?> action, CancellationToken cancel)
+        {
+            while (!cancel.IsCancellationRequested)
+            {
+                try
+                {
+                    using var pipe = new NamedPipeServerStream(PipeName);
+                    await pipe.WaitForConnectionAsync(cancel);
+
+                    using var sr = new StreamReader(pipe);
+                    var msg = await sr.ReadLineAsync();
+
+                    action.Invoke(msg);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Catch Exception at {MethodBase.GetCurrentMethod()?.Name}: {ex.Message}");
+                }
+            }
+        }
     }
 }
