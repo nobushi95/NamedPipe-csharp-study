7fcdd3da206c25c1c9554dff5e5faf27cbb18c8a
nobushi95 <nobushi1121@gmail.com>
2023-03-19 17:43:55 +0900

    サーバーコンソール実装

--- OneWayConsole/ServerConsole/Program.cs
+++ OneWayConsole/ServerConsole/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace ServerConsole
+﻿using Shared;
+
+namespace ServerConsole
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static readonly CancellationTokenSource cancel = new();
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("--- Server Console ---");
+            await Pipe.Server(OnReceive, cancel.Token);
+            Console.Write("Press enter key to exit.");
+            Console.Read();
+        }
+
+        private static void OnReceive(string? msg)
+        {
+            if (msg == "exit")
+            {
+                cancel.Cancel();
+            }
+            Console.WriteLine($"Received message: \"{msg}\"");
         }
     }
 }
