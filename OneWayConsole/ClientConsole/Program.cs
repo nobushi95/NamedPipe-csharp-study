12027fea486bdfbb7b0afaac362e04ca471b24bb
nobushi95 <nobushi1121@gmail.com>
2023-03-19 17:43:42 +0900

    クライアントコンソール実装

--- OneWayConsole/ClientConsole/Program.cs
+++ OneWayConsole/ClientConsole/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace ClientConsole
+﻿using Shared;
+
+namespace ClientConsole
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("--- Client Console ---");
+            while (true)
+            {
+                Console.Write("> ");
+                var msg = Console.ReadLine();
+                await Pipe.SendAsync(msg);
+                if (msg == "exit")
+                    break;
+            }
+            Console.WriteLine("Press enter key to exit.");
+            Console.Read();
         }
     }
 }
