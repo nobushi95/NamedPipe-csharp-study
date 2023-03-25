40afc9f5d122ef781037335ad7d2034416868aa2
nobushi95 <nobushi1121@gmail.com>
2023-03-25 21:00:51 +0900

    ClientConsoleに多重起動防止オプションを追加

--- OneWayConsole/ClientConsole/Program.cs
+++ OneWayConsole/ClientConsole/Program.cs
@@ -1,11 +1,19 @@
-using Shared;
+﻿using Shared;
 
 namespace ClientConsole
 {
     internal class Program
     {
+        private static readonly string MutexName = "OneWayConsole_ClientConsole_MutexName";
+        private static readonly SingleInstance singleInstance = new(MutexName);
         static async Task Main(string[] args)
         {
+            // 多重起動防止
+            // 短絡評価で先にSingleInstanceで判定して、Mutexを作っておく
+            // すでにプロセスが1つ以上存在すれば、-Singleオプションで呼ばれるプロセスは起動しない
+            if (singleInstance.IsRunning() && args.Any(x => x == "-Single"))
+                return;
+
             Console.WriteLine("--- Client Console ---");
             while (true)
             {
