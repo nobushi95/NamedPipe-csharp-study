bb94b8a1c53a2dc9171bbc1080a06fb9f16d150a
nobushi95 <nobushi1121@gmail.com>
2023-03-26 17:34:45 +0900

    ServerWPFの実装

--- OneWayWPF/ServerWPF/MainWindow.xaml.cs
+++ OneWayWPF/ServerWPF/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Shared;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ServerWPF
 {
@@ -20,9 +10,47 @@ namespace ServerWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly CancellationTokenSource _cancel = new();
+        private readonly SynchronizationContext? _context;
+
         public MainWindow()
         {
             InitializeComponent();
+            _context = SynchronizationContext.Current;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                await new Pipe().Server(OnReceive, _cancel.Token);
+            }).ConfigureAwait(false);
+        }
+
+        private void OnReceive(string? msg)
+        {
+            if (msg == "exit")
+            {
+                _cancel.Cancel();
+                //Close(); // 終了できない(CloseはUIスレッドで呼ばなければいけない？)
+
+                // DispatcherとSynchronizationContextで呼ぶ違いは？？
+                //_context?.Send((object? _) => { Close(); }, null);
+                Dispatcher.Invoke(() => Close());
+            }
+
+            // DispatcherとSynchronizationContextで呼ぶ違いは？？
+            // どちらもUIが更新される
+
+            //_context?.Send((object? _) =>
+            //{
+            //    ReceiveTextBlock.Text += $"{msg}\n";
+            //}, null);
+
+            Dispatcher.Invoke(() =>
+            {
+                ReceiveTextBlock.Text += $"{msg}\n";
+            });
         }
     }
 }
