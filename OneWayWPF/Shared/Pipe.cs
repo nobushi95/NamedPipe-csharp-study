4bd89dc1b32101677acc80d958aa19f716f7857a
nobushi95 <nobushi1121@gmail.com>
2023-03-26 15:40:25 +0900

    OneWayWPFソリューションを追加

---
+++ OneWayWPF/Shared/Pipe.cs
@@ -0,0 +1,53 @@
+﻿using System.IO.Pipes;
+using System.Reflection;
+
+namespace Shared
+{
+    internal class Pipe
+    {
+        private static readonly string PipeName = "NamedPipe-csharp-study_OneWayConsole_Pipe";
+
+        public static async Task SendAsync(string? msg, TimeSpan timeout = default)
+        {
+            using var pipe = new NamedPipeClientStream(PipeName);
+            try
+            {
+                if (timeout == default)
+                    await pipe.ConnectAsync();
+                else
+                    await pipe.ConnectAsync((int)timeout.TotalMilliseconds);
+
+                using var sw = new StreamWriter(pipe);
+                await sw.WriteLineAsync(msg);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Catch Exception at {MethodBase.GetCurrentMethod()?.Name}(): {ex.Message}");
+            }
+        }
+
+        public static async Task Server(Action<string?> action, CancellationToken cancel)
+        {
+            while (!cancel.IsCancellationRequested)
+            {
+                try
+                {
+                    // 複数のサーバーを立ち上げ、メッセージを送信すると、
+                    // プロセスを起動した順にメッセージが受信される？
+                    using var pipe = new NamedPipeServerStream(PipeName, PipeDirection.InOut, 3);
+                    await pipe.WaitForConnectionAsync(cancel);
+
+                    using var sr = new StreamReader(pipe);
+                    var msg = await sr.ReadLineAsync();
+
+                    action.Invoke(msg);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Catch Exception at {MethodBase.GetCurrentMethod()?.Name}: {ex.Message}");
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ OneWayWPF/Shared/SingleInstance.cs
@@ -0,0 +1,52 @@
+﻿namespace Shared
+{
+    internal class SingleInstance : IDisposable
+    {
+        private string _mutexName { get; init; }
+
+        private Mutex? _mutex = default;
+
+        public SingleInstance(string mutexName) => _mutexName = mutexName;
+
+        ~SingleInstance() => Dispose();
+
+        public bool IsRunning()
+        {
+            if (_mutex is not null)
+                return true;
+
+            try
+            {
+                _mutex = Mutex.OpenExisting(_mutexName);
+                return true;
+            }
+            catch
+            {
+                _mutex = new Mutex(true, _mutexName);
+                return false;
+            }
+        }
+
+        public async Task<bool> IsRunningAsync(TimeSpan timeout = default)
+        {
+            var cur = TimeSpan.Zero;
+            while (cur.TotalMilliseconds <= timeout.TotalMilliseconds)
+            {
+                if (IsRunning())
+                    return false;
+                await Task.Delay(TimeSpan.FromMilliseconds(100));
+                cur += TimeSpan.FromMilliseconds(100);
+            }
+            return false;
+        }
+
+        public void Dispose()
+        {
+            _mutex?.Dispose();
+            _mutex = null;
+        }
+
+    }
+
+}
+
